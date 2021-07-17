5da9cefaba2a3cce26ce43261faed0609ce7b126
Jessica Kamman <jessicakammann@Jessicas-MacBook-Air.local>
2021-07-17 09:01:57 -0700

    added first step of localStorage funciton

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-var day = moment().add(3, "hours").format('MMMM Do YYYY, h:mm:ss a');
+var day = moment().format('MMMM Do YYYY, h:mm:ss a');
 console.log(day);
 //Display current Day in header 
 $('#currentDay').html(day);
 
 //hour
-var hour = moment().add(3, "hours").format("hh:mm:ss K");
+var hour = moment().format("hh:mm:ss K");
 console.log(hour);
 
-var currentTime= moment().add(3, "hours");
+var currentTime= moment();
 
-
+$('.saveBtn').on("click", saveTask);
 
 //save button
-var saveTask =function(){
-    localStoragge.setItem();
-}
+var saveTask = function() {
+    $(this).siblings(".description").val();
+   var task= localStorage.setItem("tasks", JSON.stringify(tasks));
+    
 
-var saveTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
   };
 
 //Check if the task is in the past
@@ -35,7 +34,7 @@ var presentHour = function(){
         console.log(hourString);
          var hourMoment= moment(hourString, "h A");
         //apply new class if its after present hour
-    if (hourMoment.isSame(currentTime)){
+    if (hourMoment.isSame(currentTime, "hour")){
         //remove any old classes from element
         
     $(".time-block .col-md-10").eq(i).removeClass("bg-dark");
@@ -49,17 +48,14 @@ var presentHour = function(){
     }
 
     }
-
-
-   
+  
 }
 presentHour();
-//Future Hour
 
 
 
-//WHEN I view the time blocks for that day
-//THEN each time block is color-coded to indicate whether it is in the past, present, or future
+
+
 //WHEN I click the save button for that time block
 //THEN the text for that event is saved in local storage
 //WHEN I refresh the page
